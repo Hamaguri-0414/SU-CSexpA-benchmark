2d66d7cdd14cad65d4c19ce19b15464d802f48a0
aiba <socceraba2701@gmail.com>
2022-03-16 20:41:11 +0900

    update main.go

--- benchmarkserver/main.go
+++ benchmarkserver/main.go
@@ -1,98 +1,99 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "text/template"
-  "encoding/json"
+	"encoding/json"
 	"fmt"
-  "time"
-  "os"
-  //"reflect"
-  //reflect.TypeOf(t)
-  "benchmarkserver/internal/ab"
-  "benchmarkserver/internal/record"
-  "github.com/rs/xid"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+	"time"
+	//"reflect"
+	//reflect.TypeOf(t)
+	"benchmarkserver/internal/ab"
+	"benchmarkserver/internal/record"
+	"github.com/rs/xid"
 )
 
 // ajax戻り値のJSON用構造体
 type Param struct {
 	Time string
-  Msg string
+	Msg  string
 }
 
 func main() {
-  // webフォルダにアクセスできるようにする
-  http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./web/css/"))))
-  http.Handle("/script/", http.StripPrefix("/script/", http.FileServer(http.Dir("./web/script/"))))
-  http.Handle("/gif/", http.StripPrefix("/gif/", http.FileServer(http.Dir("./web/gif/"))))
+	// webフォルダにアクセスできるようにする
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./web/css/"))))
+	http.Handle("/script/", http.StripPrefix("/script/", http.FileServer(http.Dir("./web/script/"))))
+	http.Handle("/gif/", http.StripPrefix("/gif/", http.FileServer(http.Dir("./web/gif/"))))
 
-  //ルーティング設定 "/"というアクセスがきたら rootHandlerを呼び出す
-  http.HandleFunc("/", rootHandler)
-  http.HandleFunc("/measure", measureHandler)
+	//ルーティング設定 "/"というアクセスがきたら rootHandlerを呼び出す
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/measure", measureHandler)
 
-  log.Println("Listening...")
-  // 3000ポートでサーバーを立ち上げる
-  err := http.ListenAndServe(":3000", nil)
-  if err != nil {
-    log.Println("<Debug> http.LinstenAndServe(:3000) : ", err)
-  }
+	log.Println("Listening...")
+	// 3000ポートでサーバーを立ち上げる
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Println("<Debug> http.LinstenAndServe(:3000) : ", err)
+	}
 }
 
 //main画面
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-  //index.htmlを表示させる
+	//index.htmlを表示させる
 	tmpl := template.Must(template.ParseFiles("./web/html/index.html"))
 	err := tmpl.Execute(w, nil)
-  if err != nil {
-    log.Println("<Debug> can't open ./web/html/index.htm : ", err)
-  }
+	if err != nil {
+		log.Println("<Debug> can't open ./web/html/index.htm : ", err)
+	}
 }
 
 //フォームからの入力を処理 index.jsから受け取る
 func measureHandler(w http.ResponseWriter, r *http.Request) {
 
-  //ログファイルを開く
-  logfile := logfileOpen()
-  defer logfile.Close()
+	//ログファイルを開く
+	logfile := logfileOpen()
+	defer logfile.Close()
 
-  //index.jsに返すJSONデータ変数
-  var ret Param
-  //POSTデータのフォームを解析
-  err := r.ParseForm()
-  if err != nil {
-    log.Println("<Debug> r.ParseForm : ", err)
-  }
+	//index.jsに返すJSONデータ変数
+	var ret Param
+	//POSTデータのフォームを解析
+	err := r.ParseForm()
+	if err != nil {
+		log.Println("<Debug> r.ParseForm : ", err)
+	}
 
-  url := r.Form["url"][0]
-  groupName := r.Form["groupName"][0]
+	url := r.Form["url"][0]
+	groupName := r.Form["groupName"][0]
 
-  //idを設定(logを対応づけるため)
-  guid := xid.New()
-  log.Println("<Info> request URL: " + url + ", GroupName: " + groupName + ", id: " + guid.String())
-  fmt.Fprintln(logfile, time.Now().Format("2006/01/02 15:04:05") + "<Info> request URL: " + url + ", GroupName: " + groupName + ", id: " + guid.String())
+	//idを設定(logを対応づけるため)
+	guid := xid.New()
+	log.Println("<Info> request URL: " + url + ", GroupName: " + groupName + ", id: " + guid.String())
+	fmt.Fprintln(logfile, time.Now().Format("2006/01/02 15:04:05")+"<Info> request URL: "+url+", GroupName: "+groupName+", id: "+guid.String())
 
-  //abコマンドで負荷をかける．計測時間を返す
+	//abコマンドで負荷をかける．計測時間を返す
 	ret.Msg, ret.Time = ab.Ab(logfile, guid.String(), url)
+	//ret.Msg, ret.Time = ab.Abtest(logfile, guid.String(), url)
 
-  //計算結果を記録する
-  if ret.Msg == "" {
-    ret.Msg = record.Record(logfile, guid.String(), ret.Time, groupName)
-  }
+	//計算結果を記録する
+	if ret.Msg == "" {
+		ret.Msg = record.Record(logfile, guid.String(), ret.Time, groupName)
+	}
 
 	// 構造体をJSON文字列化する
 	jsonBytes, _ := json.Marshal(ret)
-  // index.jsに返す
-  fmt.Fprint(w, string(jsonBytes))
+	// index.jsに返す
+	fmt.Fprint(w, string(jsonBytes))
 }
 
 //ログファイルを開く，ログファイルをgithubにpushする
 func logfileOpen() *os.File {
 
-  //ログファイルを開く(logを記録するファイル)
-  logfile, err := os.OpenFile("data/log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-  if err != nil {
-    log.Println("<Debug> can't open data/log.txt : ", err)
-  }
-  return logfile
+	//ログファイルを開く(logを記録するファイル)
+	logfile, err := os.OpenFile("data/log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Println("<Debug> can't open data/log.txt : ", err)
+	}
+	return logfile
 }
